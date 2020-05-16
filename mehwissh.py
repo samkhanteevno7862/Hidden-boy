20ba04f79bd132515b3d43e61b3775747ca07d71
samkhanteevno7862 <65410664+samkhanteevno7862@users.noreply.github.com>
2020-05-16 07:48:21 +0500

    Add files via upload

---
+++ mehwissh.py
@@ -0,0 +1,8 @@
+
+print("Hi my Name is Mehwish I study in ")
+print ("Information Technology")
+print ("Im from Hydrabad Sindh Pakistan")
+print("Studying in Sindh University Jamshoro")
+print ("How are you guys Im going to show you how to make commands")
+print ("Haha ")
+print ("Yeah Im your Baaap 😂😂😂")
